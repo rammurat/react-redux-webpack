a5a62a07f1c18a9846a3d108d92ef64e10f96aeb
Ram Murat Sharma <https://ramsharm@tools.publicis.sapient.com>
2019-04-12 12:28:16 +0100

    Unwanted jquery removed and classes corrected

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header = () => (
   <header className="app-header">
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-      <h5 class="my-0 mr-md-auto font-weight-normal"><a className="navbar-brand" href="/">LOGO</a></h5>
-      <nav class="my-2 my-md-0 mr-md-3">
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+      <h5 className="my-0 mr-md-auto font-weight-normal"><a className="navbar-brand" href="/">LOGO</a></h5>
+      <nav className="my-2 my-md-0 mr-md-3">
         <a className="p-2 text-dark active" href="/company">The company <span className="sr-only">(current)</span></a>
         <a className="p-2 text-dark" href="/work">Our work</a>
         <a className="p-2 text-dark" href="/contact">Contact us</a>
